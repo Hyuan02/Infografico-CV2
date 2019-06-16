51ce4e8fe85176ff3893818211a84e3d438b3703
Hyuan Peixoto <hyuanpeixoto@gmail.com>
2019-06-15 22:52:43 -0300

    implementacao de todos os dialogos e escolhas feitos, alem de portas nos cenarios

--- dialogos.js
+++ dialogos.js
@@ -8,6 +8,119 @@ dialogo1 = {"indiceDialogo":0, "conteudo": [
     {"nome": "Ann, a Estalajadeira", "texto":"Entendo... Então você não é deste reino não é? Certo ! Vou lhe explicar. Um Jogo é uma relíquia bastante preciosa nesse reino e através dela você consegue chegar no tesouro da Experiência. Cada Jogo tem seus próprios princípios que são utilizados para sua construção. Eles variam de acordo com cada um que os constroem, como por exemplo regras, objetivos, valor endógeno, imersão, interação entre outros. Com esses princípios, cada Jogo pode chegar na Experiência."},
     {"nome": "Ann, a Estalajadeira", "texto":"A Experiência é uma magia muito poderosa, capaz de mexer com as pessoas e é considerada o maior tesouro para todos os usuários de Jogos. Ela é capaz de nos proporcionar várias sensações e por isso é tão almejada nesse reino. Algumas Experiências podem nos proporcionar diversão, umas podem nos fazer fugir de nossa realidade e outras permitem que façamos o que desejarmos. São muitas as Experiências que podemos buscar."},
     {"nome": "Ann, a Estalajadeira", "texto":" Agora que você já sabe o que é um Jogo, deve construir um para conseguir sua Experiência. Você tem que planejar como quer que ele seja, essa etapa se chama Projeto. Com o Projeto feito e documentado, poderá construir seu Jogo através da Prototipação. Primeiro, vamos definir que tipo de Experiência você quer conseguir com seu Jogo. Existem jogos específicos para cada Experiência que se deseja conseguir, então escolha com sabedoria o que você deseja e eu lhe darei um mapa para ela!"},
-    {"alternativas":[ {"valor":"Diversão", "selecionado":false}, {"valor":"Fuga", "selecionado":false}, {"valor":"Prazer", "selecionado":false}, {"valor":"Livre Escolha", "selecionado":false}]},
+    {"alternativas":[ {"valor":"Diversão", "selecionado":false}, {"valor":"Fuga", "selecionado":false}, {"valor":"Prazer", "selecionado":false}, {"valor":"Livre Escolha", "selecionado":false}], "imagens":["quest.png"]},
     {"nome": "Ann, a Estalajadeira", "texto":"Perfeito! Vamos começar os preparativos para a construção do seu Jogo. Agora você precisa dos Elementos para construir o Jogo, fale com o Rudolph, o Mago. Ele saberá como te ajudar."}
 ],"aconteceu": false};
+
+dialogo2 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Rudolph, o Mago", "texto":"Olá jovem aventureiro, o que deseja ?"},
+    {"nome": "Jogador", "texto":"..."},
+    {"nome": "Rudolph, o Mago", "texto":"Entendo... Então você deseja construir um Jogo para conseguir a Experiência (colocar o nome da Experiência escolhida)? Muito bem. Não pode construir um Jogo sem antes escolher os seus Elementos."},
+    {"nome": "Jogador", "texto":"..."},
+    {"nome": "Rudolph, o Mago", "texto":"O que ? Você quer o Elemento Fogo ? Ha ha ha ... Não meu jovem, aqui os Elementos são diferentes. Os 4 Elementos são cruciais para a construção do Jogo, pois funcionam como o esqueleto do Jogo. Eles são: Mecânica, Narrativa, Estética e Tecnologia. E estão sempre interligados, sendo um deles o principal núcleo do seu Jogo."},
+    {"nome": "Jogador", "texto":"...."},
+    {"nome": "Rudolph, o Mago", "texto":" A Mecânica é o Elemento do objetivo, das regras e possibilidades. É ela que define o Jogo em si. A Narrativa é o Elemento da história e dos eventos. Um jogo poderá conter ou não uma história. Caberá a você decidir se ela será relevante ou não. A Estética é o Elemento responsável pela aparência, sensações, sons, cheiros e sabores do Jogo. Sendo ela, o Elemento mais ligado à Experiência. Por fim, a Tecnologia é o Elemento dos materiais e interações que tornaram seu Jogo possível."},
+    {"nome": "Rudolph, o Mago", "texto":"Ao escolher um desses elementos como núcleo, os outros três serão utilizados para reforçar as características do núcleo do seu Jogo. Agora meu jovem, você me parece um aventureiro bastante promissor e por isso lhe darei um dos núcleos como um presente para sua jornada. Qual você deseja escolher?"},
+    {"nome": "Jogador", "texto":"..."},
+    {"alternativas":[{"valor":"Núcleo da Mecânica", "selecionado":false}, {"valor":"Núcleo da Narrativa", "selecionado":false}, {"valor":"Núcleo da Estética", "selecionado":false}, {"valor":"Núcleo da Tecnologia", "selecionado":false}], "imagens":["esfera1.png","esfera2.png"]},
+    {"nome": "Rudolph, o Mago", "texto":"Boa escolha, agora deverá escolher um Tema. Creio que Amathea, a aventureira poderá lhe guiar nisso, ela possui excelentes histórias. Você pode encontrá-la na biblioteca."}
+], "aconteceu":false};
+
+dialogo3 = { "indiceDialogo":0, "conteudo":[
+    {"nome":"Amathea, a Aventureira", "texto":"Olá caro aventureiro, procurando dicas de aventura?"},
+    {"nome":"Jogador", "texto":"..."},
+    {"nome":"Amathea, a Aventureira", "texto":"O que é um Tema? Ora, é aquilo que o jogo é. Uma ideia que interliga todo o jogo, capaz de envolver mais as pessoas. Você pode criar Jogos com nenhum Tema ou contendo Temas frágeis, mas Jogos com Temas unificadores e ressonantes conseguiram Experiências mais intensas."},
+    {"nome":"Amathea, a Aventureira", "texto":"Um Tema unificador foca os recursos do seu jogo na direção de um objetivo e um Tema ressonante é aquele que toca profundamente as pessoas. Nem todo Jogo precisa ter um Tema ressonante, pois mesmo que não tenha um ainda será reforçado por um Tema unificador."},
+    {"nome":"Jogador", "texto":"..."},
+    {"nome":"Amathea, a Aventureira", "texto":"Como escolher um Tema? Bom, você pode fazer um Jogo sobre tudo, qualquer história. Histórias, objetos, memórias, tudo pode ajudar você na escolha do seu Tema. Como uma vez em que derrotei uma tripulação de piratas e como recompensa recebi um baú cheio de tesouros. Ou outra em que achei um tesouro muito antigo num templo escondido por uma imensa floresta. Já sei ! Por que não dá uma olhada nos livros da biblioteca e depois vem falar comigo? Assim, poderemos ter algumas ideias sobre o seu tema."},
+],
+"aconteceu" : false
+};
+
+dialogo4 = {"indiceDialogo":0, "conteudo":[
+    {"nome":"Amathea, a Aventureira", "texto":"Qual Tema você gostaria de escolher?"},
+    {"alternativas":[{"valor":"Tema Pirata", "selecionado":false },{"valor":"Tema Mitológico", "selecionado":false },{"valor":"Tema Espacial", "selecionado":false },{"valor":"Tema Culinário", "selecionado":false}]},
+    {"nome":"Amathea, a Aventureira", "texto":"Que maravilhoso! Seu Jogo vai ser incrível! Agora que você tem um Tema, poderá desenvolver a Ideia do seu Jogo. Vá para o bar falar com Mary, a bartender. Ela é ótima para se debater algumas ideias."},
+
+], "aconteceu": false};
+
+dialogo5 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Mary, a Bartender", "texto":"Soube pela Ann, a Estalajadeira que você está querendo construir um Jogo. Do que precisa?"},
+    {"nome": "Jogador", "texto":"..."},
+    {"nome": "Mary, a Bartender", "texto":"Então você já tem tudo isso, mas precisa de uma Ideia para construir seu Jogo. Bom, ideias em sua maioria não surgem do nada. É necessário pensar bastante. Uma sugestão é formular um problema. Formulando um problema deixará mais claro qual o objetivo do seu Jogo e as limitações que ele terá."},
+    {"nome": "Mary, a Bartender", "texto":"Tendo seu problema você terá que pensar numa solução. Para isso é só seguir o processo de Brainstorming. Nesse processo você irá discutir e pensar em novas ideias com outras pessoas, sem críticas apenas produzindo o máximo de ideias que conseguirem. Terminado esse processo, você poderá filtrar as ideias produzidas e escolher as que parecem melhores para o seu Jogo. Você pode anotar ideias, desenhar, brincar, se concentrar, fazer qualquer coisa que lhe deixe confortável para torná-lo mais produtivo. Por que não conversa com algumas moradores que estão pelo bar para debater algumas ideias? Eles adoram saber sobre os Jogos que os aventureiros estão planejando construir. Quando terminar venha falar comigo."}
+], "aconteceu": false};
+
+dialogo6 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Mary a Bartender", "texto":"Qual ideia você gostaria de escolher?"},
+    {"alternativas":[{"valor":"Coletar Itens", "selecionado":false },{"valor":"Derrotar Monstros", "selecionado":false },{"valor":"Caverna de Misterios", "selecionado":false },{"valor":"Criar coisas", "selecionado":false}]},
+    {"nome": "Mary a Bartender", "texto":"Agora você tem bastante coisa não? Não esqueça de documentar para não perder."},
+    {"nome": "Jogador", "texto":"...."},
+    {"nome": "Mary a Bartender", "texto":"Você não sabe o que é documentar?! Documentar é registrar tudo o que você produziu e pensou a respeito do seu Jogo. Para isso criamos o GDD ou Game Design Document. No GDD estará constado tudo sobre o seu Jogo, desde a Ideia, Tema, Narrativa, Fases produzidas, Mecânicas, tudo. Para que no futuro você não esqueça o que estava sendo feito no seu Jogo. Ele servirá também como meio de comunicação para que outras pessoas possam saber como é o seu Jogo. Você pode criar seu GDD com a Ann, a Estalajadeira. Basta escrever no papel que está em cima do seu balcão. Depois de documentar seu Jogo fale com a Ann, a Estalajadeira, ela saberá como lhe guiar."}
+], "aconteceu":false};
+
+dialogo7 = {"indiceDialogo":0, "conteudo":[
+    {"nome":"Ann, a Estalajadeira", "texto":"Ah! Já conseguiu tudo isso ? Que notícia maravilhosa, está mais perto de criar seu Jogo. Antes de produzir seu Protótipo, vamos verificar alguns passos. Afinal, você deverá se preparar para os Jogadores."},
+    {"nome":"Jogador", "texto":"..."},
+    {"nome":"Ann, a Estalajadeira", "texto":"Ah! Os Jogadores são os guardiões da Experiência. É através deles que você conseguirá chegar no tão esperado tesouro que é a Experiência. Você deve conhecer bem cada tipo de Jogador, para saber se o seu Jogo consegue se encaixar nos perfis deles. Que tal falar com os aprendizes novamente?"},
+]};
+
+dialogo8 = {"indiceDialogo":0, "conteudo":[
+    {"nome":"Ann, a Estalajadeira", "texto":" Agora que já sabe sobre os Jogadores, vamos dar uma olhada nas Mecânicas. Que tal relembrar ? As Mecânicas são comportamentos, regras ou módulos que podem mudar o estado do Jogo. Elas podem ser primárias, quando usadas diretamente, ou secundárias, quando são usadas esporadicamente. Vejo que escolheu boas Mecânicas, por que não escolhe mais uma para deixar seu Jogo mais forte?"},
+    {"alternativas":[{"valor":"Mecânica de Administração de Recursos", "selecionado":false },{"valor":"Mecânica de Memória", "selecionado":false },{"valor":"Mecânica de Coleta de Itens", "selecionado":false },{"valor":"Mecânica de Negociação", "selecionado":false}]},
+    {"nome":"Ann, a Estalajadeira", "texto":"Perfeito! Agora podemos construir seu Protótipo. O Protótipo é o que virá a ser seu Jogo finalizado. Ao montarmos o Protótipo, você poderá testar todos os elementos que escolheu para construir seu Jogo. Caso ocorra algum problema ou impedimento, você poderá sempre voltar para a etapa de Brainstorming para ver como poderá corrigir tais problemas."},
+    {"nome":"Ann, a Estalajadeira", "texto":"O processo a ser realizado no seu Protótipo para a correção desses problemas é o Balanceamento. Através do Balanceamento problemas como pontuações altas demais, desafios impossíveis, habilidades desiguais, inimigos muito fortes ou fracos podem ser resolvidos. Existem vários tipos de Balanceamento, mas os mais comuns são os seguintes:"},
+    {"lista":["Equidade, que diz respeito às vantagens e desvantagens dentro de um Jogo","Desafio e Sucesso, que diz respeito a quantidade de desafios e sucessos dos mesmos dentro de um Jogo;","Escolhas significativas, que diz respeito sobre as escolhas que podem ser tomadas dentro de um Jogo;","Habilidade e Probabilidade, que diz respeito sobre o quanto de habilidade ou probabilidade será utilizado dentro de um Jogo;","Corpo e Mente, que diz respeito sobre o quanto de habilidade física ou mental deverá ser utilizado dentro de um Jogo;", "Competição e Cooperação, que diz respeito se o Jogo será competitivo, cooperativo ou uma mistura de ambos;","Curto e Longo, que diz respeito ao tempo de duração da partida de um Jogo;","Recompensas, que diz respeito sobre como o jogador teve um bom desempenho dentro de um Jogo e como irá recompensá-lo por isso;","Punição, que diz respeito sobre como o jogador deverá ser castigado por adotar tal comportamento;","Liberdade e Experiência Controlada, que diz respeito ao quanto de controle será dado ao jogador dentro de um Jogo;","Simples e Complexo, que diz respeito ao nível de complexidade dentro de um Jogo;","Detalhe e Imaginação, que diz respeito ao equilíbrio entre esses dois aspectos para a experiência do jogador;"]},
+    {"nome":"Ann, a Estalajadeira","texto":"O Balanceamento é extremamente importante para que o Jogo não se tornear desagradável. Tudo checado, agora finalmente você tem seu Jogo e já pode sair para buscar o grande tesouro da Experiência. Não esqueça de documentar antes de sair!"}
+]};
+
+
+
+dialogoLivro1 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 1", "texto":" Aqui contém alguns livros sobre receitas e poções"}
+], "aconteceu" : false};
+dialogoLivro2 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 2", "texto":"Uma bela epopéia sobre um guerreiro Llama em busca de sua amada Alpaca"}
+], "aconteceu" : false};
+dialogoLivro3 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 3", "texto":" \"Administrando sua fazenda de lesmas: Tudo o que você precisa saber e o que não precisa saber para administrar uma fazenda de lesmas, vol. 1 a 25\""}
+], "aconteceu" : false};
+dialogoLivro4 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 4", "texto":" \"As faces do blefe: como consegui ser o maior jogador de poker\" escrito por João Duas Caras"}
+], "aconteceu" : false};
+dialogoLivro5 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 5", "texto":"\"Kraken e eu\", uma linda história de amor e intrigas sobre um pirata que se apaixona por um Kraken, um clássico"},
+], "aconteceu" : false};
+dialogoLivro6 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Estante 6", "texto":"\"Jornada dos céus\", uma história divertida sobre duas amigas estrelas visitando outros planetas"},
+], "aconteceu" : false};
+
+
+dialogoGenerico1 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Um Jogo em que eu tenha que correr pra coletar itens seria o que eu faria"},
+], "aconteceu" : false};
+dialogoGenerico2 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Derrotar monstros gigantes para ficar mais poderoso e ganhar mais tesouros"},
+], "aconteceu" : false};
+dialogoGenerico3 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Eu criaria meu Jogo para explorar uma caverna cheia de mistérios em busca de um tesouro muito raro"},
+], "aconteceu" : false};
+dialogoGenerico4 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Com meu Jogo eu criaria armas, feitiços e poções incríveis e muito poderosas"},
+], "aconteceu" : false};
+
+dialogoGenerico5 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Os Realizadores são um tipo de Jogador que gosta de alcançar objetivos e de resolverem desafios."},
+], "aconteceu" : false};
+dialogoGenerico6 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Os Exploradores são um tipo de Jogador que gosta de todo o jogo ou seu mapa para descobrir novas coisas. Jogos que permitam a exploração de áreas irá agradar bastante esse perfil."},
+], "aconteceu" : false};
+dialogoGenerico7 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Os Socializadores são um tipo de Jogador que gosta de se relacionar com outras pessoas. O companherismo é importante para esse tipo de Jogador. Jogos com o quesito de cooperatividade irá agradar bastante esse perfil."},
+], "aconteceu" : false};
+dialogoGenerico8 = {"indiceDialogo":0, "conteudo":[
+    {"nome": "Aprendiz", "texto":"Os Predadores são um tipo de Jogador que gostam de competir com outras pessoas. Também gostam de destruir coisas e serem bastante influenciadores. Jogos que estimulem a competição ou que permita influência do jogo ou de jogadores irão agradar bastante esse perfil."},
+], "aconteceu" : false};
+
+
+
--- main2.js
+++ main2.js
@@ -1,23 +1,30 @@
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
-let personagem, personagemMovimento = true, limitesCenario, bounds, composicaoCenario, porta1, porta2, bot1, caixaTexto, texto, alternativas;
-var indiceSelecionado = 0, perguntaAtual = [], dialogoAtual;
-var escolha1;
+let personagem, personagemMovimento = true, limitesCenario, bounds, composicaoCenario, porta1, porta2,porta3,porta4,bot1, bot2, bot3,bot4,bot5,caixaTexto, texto, alternativas;
+var indiceSelecionado = 0, perguntaAtual = [], dialogoAtual, dialogoAnn = 0;
+var checaLivros = false,livros;
+var checaGenericos = false, genericos, segundoDialogoGenerico = false;
 
 
 
 
 function preload() {
     game.load.image('personagemTop', './personagemTop.png');
+    game.load.spritesheet('personagemBaixo','./sprites/player/andando_frente.png',100,200,3);
+    game.load.spritesheet('personagemCima','./sprites/player/andando_tras.png',100,200,3);
+    game.load.spritesheet('personagemEsquerda','./sprites/player/andando_esquerda.png',100,200,3);
+    game.load.spritesheet('personagemDireita','./sprites/player/andando_direita.png',100,200,3);
     game.load.image('imgCenario', './imgCenario.png');
     game.load.image('imgCenario2', './cenario2.png');
+    game.load.image('imgCenario3', './cenario3.png');
     game.load.image('porta', './porta.png');
     game.load.image('bot', './bot.png');
-
+    game.load.image('livro','./livro_teste.png');
+    game.load.image('generico','./generico.png');
 }
 
 function create() {
     composicaoCenario = game.add.group();
-    personagem = game.add.sprite(400, 300, 'personagemTop');
+    personagem = game.add.sprite(400, 300, 'personagemBaixo');
     personagem.scale.x = 0.3;
     personagem.scale.y = 0.3;
     alternativas = game.add.group();
@@ -41,21 +48,30 @@ function update() {
 function controles() {
     if(personagemMovimento){
         if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            if (personagem.y > 10)
+            if (personagem.y > 10){
                 personagem.y -= 5;
+            }
+
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-            if (personagem.y < 680)
+            if (personagem.y < 680){
                 personagem.y += 5;
+            } 
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
             if (personagem.x > 0)
+            {
                 personagem.x -= 5;
+            }
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
             if (personagem.x < 1280 - personagem.width)
+            {
                 personagem.x += 5;
+            }              
+            
         }
+
     }
     else{
         if(alternativas.length>1){
@@ -98,24 +114,73 @@ function hideBox() {
 
 function checarColisao() {
     if (personagem.alive) {
-        if (checkOverlap(personagem, porta1)) {
-            cenario2();
+        if(porta1){
+            if (checkOverlap(personagem, porta1)) {
+                cenario2();
+            }
+    
+            if ((checkOverlap(personagem, bot1)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))) {
+                if(dialogoAnn ==0){
+                    entraDialogo(dialogo1);
+                }
+                
+                else if(dialogoAnn == 1){
+                    entraDialogo(dialogo7);
+                    segundoDialogoGenerico = true;
+                }
+                   
+                else if(dialogoAnn == 2){
+                    entraDialogo(dialogo8);
+                }
+            }
         }
 
-        if ((checkOverlap(personagem, bot1)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))) {
-            entraDialogo(dialogo1);
-            
+        if(porta2){
+
+            if (checkOverlap(personagem, porta2)) {
+                cenario3();
+            }
+            if (checkOverlap(personagem, porta4)) {
+                cenario1();
+            }
+            if ((checkOverlap(personagem, bot2)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))) {
+                entraDialogo(dialogo2);
+            }
+
+            if ((checkOverlap(personagem, bot4)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))) {
+                if(!checaGenericos)
+                    entraDialogo(dialogo5);
+                else{
+                    entraDialogo(dialogo6);
+                    dialogoAnn = 1;
+                }
+                    
+            }
+
+            checaColisaoGenericos();
         }
-    }
-    if (porta2) {
-        if (checkOverlap(personagem, porta2)) {
-            cenario1();
+
+        if (porta3) {
+            if (checkOverlap(personagem, porta3)) {
+                cenario2();
+            }
+            if ((checkOverlap(personagem, bot3)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))) {
+                if(!checaLivros)
+                    entraDialogo(dialogo3);
+                else
+                    entraDialogo(dialogo4);
+            }
+            checaColisaoLivros();
         }
+
+
+
     }
+    
 }
 
 function cenario1() {
-    composicaoCenario.removeAll(); //limpa todos os elementos do cenario antes de compor o outro
+    composicaoCenario.removeAll(true); //limpa todos os elementos do cenario antes de compor o outro
     limitesCenario = composicaoCenario.create(640, 360, 'imgCenario');
     limitesCenario.anchor.x = 0.5;
     limitesCenario.anchor.y = 0.5;
@@ -126,21 +191,53 @@ function cenario1() {
 }
 
 function cenario2() {
-    composicaoCenario.removeAll(); //limpa todos os elementos do cenario antes de compor o outro
+    composicaoCenario.removeAll(true); //limpa todos os elementos do cenario antes de compor o outro
+    limitesCenario = composicaoCenario.create(640, 360, 'imgCenario3');
+    limitesCenario.anchor.x = 0.5;
+    limitesCenario.anchor.y = 0.5;
+    limitesCenario.width = 1280;
+    limitesCenario.height = 720;
+    genericos = game.add.group();
+    for(let i=0; i<4; i++){
+        if(i%2 == 0)
+        genericos.create(100 + (50*i), 100, "generico");
+        else
+        genericos.create(100, 100 + (50*i), "generico");
+    }
+    porta2 = composicaoCenario.create(1100, 200, 'porta');
+    porta4 = composicaoCenario.create(50,500,'porta');
+    bot2 = composicaoCenario.create(800,650,"bot");
+    bot4 = composicaoCenario.create(300,650,'bot');
+    composicaoCenario.add(genericos);
+
+}
+
+function cenario3(){
+    composicaoCenario.removeAll(true); //limpa todos os elementos do cenario antes de compor o outro
+    livros = game.add.group();
     limitesCenario = composicaoCenario.create(640, 360, 'imgCenario2');
     limitesCenario.anchor.x = 0.5;
     limitesCenario.anchor.y = 0.5;
     limitesCenario.width = 1280;
     limitesCenario.height = 720;
-    porta2 = composicaoCenario.create(400, 450, 'porta');
+    porta3 = composicaoCenario.create(400, 450, 'porta');
+    bot3 = composicaoCenario.create(200,100,"bot");
+    for(let i= 0; i<6; i++){
+        livros.create(1000,100+(50*i), "livro");
+    }
+    composicaoCenario.add(livros);
+    
 }
 
 function checkOverlap(spriteA, spriteB) {
 
-    var boundsA = spriteA.getBounds();
-    var boundsB = spriteB.getBounds();
+    if(spriteA.exists && spriteB.exists){
+        var boundsA = spriteA.getBounds();
+        var boundsB = spriteB.getBounds();
 
-    return Phaser.Rectangle.intersects(boundsA, boundsB);
+        return Phaser.Rectangle.intersects(boundsA, boundsB);
+    }
+    
 
 }
 
@@ -167,6 +264,15 @@ function entraDialogo(dialogo){
                 texto.setText(dialogo.conteudo[dialogo.indiceDialogo].texto);
                 dialogo.indiceDialogo++;
             }
+            else if('lista' in dialogo.conteudo[dialogo.indiceDialogo]){
+                texto.visible = true;
+                let textString = "";
+                for(item of dialogo.conteudo[dialogo.indiceDialogo].lista){
+                    textString+= item+"\n";
+                }
+                texto.setText(textString);
+                dialogo.indiceDialogo++;
+            }
         }
         else {
             texto.visible = false;
@@ -175,6 +281,11 @@ function entraDialogo(dialogo){
         }
  
 
+    }
+    else{
+        texto.setText(dialogo.conteudo[dialogo.indiceDialogo - 1].texto);
+        texto.visible = !texto.visible;
+        personagemMovimento = !personagemMovimento;
     }
     
 
@@ -200,4 +311,76 @@ function selecionaOpcao(){
 function encerraOpcoes(dialogo){
     alternativas.removeAll(true);
     dialogo.indiceDialogo++;
+    perguntaAtual = [];
+}
+
+function checaColisaoLivros(){
+    if((checkOverlap(personagem, livros.getAt(0)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro1);
+    }
+    if((checkOverlap(personagem, livros.getAt(1)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro2);
+    }
+    if((checkOverlap(personagem, livros.getAt(2)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro3);
+    }
+    if((checkOverlap(personagem, livros.getAt(3)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro4);
+    }
+    if((checkOverlap(personagem, livros.getAt(4)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro5);
+    }
+    if((checkOverlap(personagem, livros.getAt(5)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaLivros = true;
+        entraDialogo(dialogoLivro6);
+    }
+}
+
+function checaColisaoGenericos(){
+    if((checkOverlap(personagem, genericos.getAt(0)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaGenericos = true;
+        if(!segundoDialogoGenerico)
+            entraDialogo(dialogoGenerico1);
+        else{
+            dialogoAnn = 2;
+            entraDialogo(dialogoGenerico5);
+        }
+            
+        
+    }
+    if((checkOverlap(personagem, genericos.getAt(1)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaGenericos = true;
+        if(!segundoDialogoGenerico)
+            entraDialogo(dialogoGenerico2);
+        else{
+            dialogoAnn = 2;
+            entraDialogo(dialogoGenerico6);
+        }
+            
+    }
+    if((checkOverlap(personagem, genericos.getAt(2)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaGenericos = true;
+        if(!segundoDialogoGenerico)        
+            entraDialogo(dialogoGenerico3);
+        else{
+            dialogoAnn = 2;
+            entraDialogo(dialogoGenerico7);
+        }
+            
+    }
+    if((checkOverlap(personagem, genericos.getAt(3)) && (game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)))){
+        checaGenericos = true;
+        if(!segundoDialogoGenerico)        
+            entraDialogo(dialogoGenerico4);
+        else{
+            dialogoAnn = 2;
+            entraDialogo(dialogoGenerico8);
+        }
+            
+    }
 }
